3cff4cf1c1e4d0bef1ded668fc05c4c8186c6bdf
Joost Schuur <jschuur@jschuur.com>
2022-12-24 14:30:51 +0000

    fix: rss feed

--- apps/site/src/pages/feed.xml.js
+++ apps/site/src/pages/feed.xml.js
@@ -1,22 +1,20 @@
-import rss from "@astrojs/rss";
-import { readAll } from "../lib/markdoc/read";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
+import rss from '@astrojs/rss';
+import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from '../config';
+import { readAll } from '../lib/markdoc/read';
 
 export const get = async () => {
   const posts = await readAll({
-    directory: "blog",
+    directory: 'blog',
   });
 
   const sortedPosts = posts.sort(
-    (a, b) =>
-      new Date(b.frontmatter.date).valueOf() -
-      new Date(a.frontmatter.date).valueOf()
+    (a, b) => new Date(b.frontmatter.date).valueOf() - new Date(a.frontmatter.date).valueOf()
   );
 
   let baseUrl = SITE_URL;
   // removing trailing slash if found
   // https://example.com/ => https://example.com
-  baseUrl = baseUrl.replace(/\/+$/g, "");
+  baseUrl = baseUrl.replace(/\/+$/g, '');
 
   const rssItems = sortedPosts.map(({ frontmatter, slug }) => {
     if (frontmatter.external) {
